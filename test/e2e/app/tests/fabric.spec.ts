23fd4904a073592fc257673e418a96d044697460
Martin Reinhardt <contact@martinreinhardt-online.de>
2017-03-30 18:47:03 +0200

    test(iOS): Adding test for #46

--- test/e2e/app/tests/fabric.spec.ts
+++ test/e2e/app/tests/fabric.spec.ts
@@ -5,5 +5,9 @@ describe('Fabric', () => {
   it('should init', () => {
     Fabric.init();
   });
+  // see https://github.com/hypery2k/nativescript-fabric/issues/46
+  it('should log', () => {
+    Fabric.logError({},'msg');
+  });
 
 });
