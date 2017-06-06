4489b00bb7d3742e15a7928c6edec2a29709552c
Reinhardt, Martin <contact@martinreinhardt-online.de>
2017-06-06 08:53:10 +0200

    tests(end2end): Improve end2end test project

--- test/e2e/app/main.aot.ts
+++ test/e2e/app/main.aot.ts
@@ -1,6 +1,13 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScript } from "nativescript-angular/platform-static";
-
+import { Fabric } from 'nativescript-fabric';
 import { AppModuleNgFactory } from "./app.module.ngfactory";
 
-platformNativeScript().bootstrapModuleFactory(AppModuleNgFactory);
+try {
+  Fabric.init();
+  platformNativeScript().bootstrapModuleFactory(AppModuleNgFactory);
+  Fabric.logCustomEvent('Init done', null);
+} catch (e) {
+  Fabric.logError(e);
+  throw e;
+}
--- test/e2e/app/main.ts
+++ test/e2e/app/main.ts
@@ -1,12 +1,12 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { AppModule } from "./app.module";
 import { Fabric } from 'nativescript-fabric';
+import { AppModule } from "./app.module";
 
-platformNativeScriptDynamic().bootstrapModule(AppModule);
 try {
   Fabric.init();
-platformNativeScriptDynamic().bootstrapModule(AppModule);
+  platformNativeScriptDynamic().bootstrapModule(AppModule);
+  Fabric.logCustomEvent('Init done', null);
 } catch (e) {
   Fabric.logError(e);
   throw e;
